50d99c86ad05bdab3ce5ce98cc217b289f1b9eff
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-11-17 13:10:08 +0100

    Add files via upload

---
+++ client/include/gallery.php
@@ -0,0 +1,41 @@
+<link href="assets/css/gallery.css" rel="stylesheet">
+<div class="row center">
+<ul>
+ <li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/13.png")'></div>
+<h4 class="title">13</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/aer.png")'></div>
+<h4 class="title">aer</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/alien.png")'></div>
+<h4 class="title">alien</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/aqua.png")'></div>
+<h4 class="title">aqua</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/arac.png")'></div>
+<h4 class="title">arac</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/badg.png")'></div>
+<h4 class="title">badg</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/basil.png")'></div>
+<h4 class="title">basil</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/bestf.png")'></div>
+<h4 class="title">bestf</h4>
+</li>
+<li class="skin" onclick="$('#nick').val('{' + $(this).find('.title').text() + '} ');" data-dismiss="modal">
+<div class="circular" style='background-image: url("skins/bikr.png")'></div>
+<h4 class="title">bikr</h4>
+</li>
+</ul>
+</div>
